56bb3fe745d42f6bfb5b40474dfcff7ecbae43d1
blitz <blitz@211.217.41.59>
2022-05-29 19:54:52 +0900

    0529 프로젝트 시범생성

---
+++ Bit-forRest/src/main/java/com/mvc/forrest/BitForRestApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.forrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitForRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitForRestApplication.class, args);
+	}
+
+}
